6b21e64a1c966996daf8f85309826eb6b25d49c8
Hsiu-Fan Wang <hfwang@porkbuns.net>
2016-04-23 00:29:05 -0700

    Initial commit

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: dev_appserver.py --skip_sdk_update_check=yes --port=$PORT --api_port=$(($PORT + 200)) --admin_port=$(($PORT + 100)) --host=0.0.0.0 app.yaml
+# webpack: node_modules/.bin/webpack --watch
+# rake: rake watch --trace
---
+++ main.py
@@ -0,0 +1,63 @@
+import datetime
+import uuid
+import simplejson
+
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
+from six.moves.urllib.error import HTTPError
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+app.secret_key = "7W9YZ75HHZZR6RB8"
+app.config['DEBUG'] = True
+
+
+credentials = GoogleCredentials.get_application_default()
+bigquery = build('bigquery', 'v2', credentials=credentials)
+
+
+@app.route('/')
+def index():
+    """Return a friendly HTTP greeting."""
+    return "Hello World"
+
+
+# @app.route('/e/<project_id>/<dataset_id>', methods=["GET"])
+# def list_tables(project_id, dataset_id):
+#     """List dataset's tables."""
+#     return "%r" % bigquery.datasets().list(projectId=project_id).execute()
+
+
+@app.route('/e/<project_id>/<dataset_id>/<table_id>', methods=["POST"])
+def insert_event(project_id, dataset_id, table_id):
+    """Insert events."""
+    suffix = datetime.date.today().strftime("%Y%m%d")
+
+    data = []
+    rows = [request.json] if type(request.json) == type(dict()) else request.json
+    for row in rows:
+        insert_id = row["_id"]["$oid"]
+        event_key = row["eventKey"]
+        player_id = row["playerID"]
+        timestamp = row["timestamp"]
+        body = simplejson.dumps(row)
+        data.append({
+            "json": {
+                "event_key": event_key,
+                "player_id": player_id,
+                "timestamp": timestamp,
+                "data": body
+            },
+            "insertId": insert_id
+        })
+    res = bigquery.tabledata().insertAll(
+        projectId=project_id,
+        datasetId=dataset_id,
+        tableId=table_id,
+        body={
+            "rows": data,
+            "templateSuffix": suffix
+        }).execute()
+    return "%r" % res
